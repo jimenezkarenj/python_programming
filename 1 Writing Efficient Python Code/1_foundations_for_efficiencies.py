f3bb4d8657ab31e3f321be34e4c3780db29684a6
jimenezkarenj <jimenezkarenj@gmail.com>
2020-09-01 17:32:00 -0500

    finished first chapter

--- 1 Writing Efficient Python Code/1_foundations_for_efficiencies.py
+++ 1 Writing Efficient Python Code/1_foundations_for_efficiencies.py
@@ -45,6 +45,7 @@ print(nums_list)
 
 # Create a new list of odd numbers from 1 to 11 by unpacking a range object
 nums_list2 = [*range(1,12,2)]
+print(type(nums_list2))
 print(nums_list2)
 
 """You can convert the range object into a list by using the list() function or by unpacking it into a list using the star character (*)."""
@@ -68,9 +69,9 @@ print(indexed_names_comp)
 indexed_names_unpack = [*enumerate(names, 1)]
 print(indexed_names_unpack)
 
-####################################
+##############################
 ## Built-in practice: map() ##
-####################################
+##############################
 
 # Use map to apply str.upper to each element in names
 names_map  = map(str.upper, names)
@@ -83,3 +84,53 @@ names_uppercase = [* names_map]
 
 # Print the list created above
 print(names_uppercase)
+
+##################################
+## Practice with NumPy arrays() ##
+##################################
+
+import numpy as np
+
+nums = np.array([[ 1,  2,  3,  4,  5], [ 6,  7,  8,  9, 10]])
+
+# Print second row of nums
+print(nums[1,:])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums
+nums[:, 2] = nums[:, 2] + 1
+print(nums)
+
+"""Using numpy arrays allows you to take advantage of an array's memory efficient nature and easily perform mathematical operations on your data."""
+
+
+##############################
+## Bringing it all together ##
+##############################
+
+# Create a list of arrival times
+arrival_times = [*range(10,60,10)]
+
+# Convert arrival_times to an array and update the times
+arrival_times_np = np.array(arrival_times)
+new_times = arrival_times_np - 3
+
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i],time) for i, time in enumerate(new_times)]
+
+def welcome_guest(self):
+    welcome = str(f"Welcome to Festivus {self[0]}... You're {self[1]} min late.")
+    return welcome
+
+#Map the welcome_guest function to each (guest,time) pair
+welcome_map = map(welcome_guest, guest_arrivals)
+
+guest_welcomes = [*welcome_map]
+print(*guest_welcomes, sep='\n')
